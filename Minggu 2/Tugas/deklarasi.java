c8a112aff1ff03fdf03c2331c055b539e5384ae5
farulwananda <farulahmad2002@gmail.com>
2021-03-16 17:38:17 +0700

    Create deklarasi.java

---
+++ Minggu 2/Tugas/deklarasi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+
+/**
+ *
+ * @author farul
+ */
+public class deklarasi {
+    public static void main (String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi variabel yang tidak benar ( salah )
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+        
+        
+        //Deklarasi variabel yang benar
+        boolean gameOver = false;
+        int students = 50, ckasses = 3;
+        double sales_tax;
+        short number1;
+    }
+}
