3031bcaf0cff9483f0379e808cf40f63422d74e2
farulwananda <farulahmad2002@gmail.com>
2021-03-23 17:48:32 +0700

    Create TugasA.java

---
+++ Minggu 3/Tugas/TugasA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas2;
+
+/**
+ *
+ * @author farul
+ */
+public class TugasA {
+    public static void main(String[] Args) {
+        //Tugas A Array 1 Dimensi
+        
+       String[] nama = {"Farul","Ahmad","Wananda"};
+       
+       for(int j=0; j<nama.length; j++){
+           System.out.print(nama[j]);
+           System.out.print(" ");
+       }
+    }
+}
