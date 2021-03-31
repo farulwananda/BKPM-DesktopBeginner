3fc4bd3837ead23da8e49bbaa6c1c792d1ae4fa9
farulwananda <farulahmad2002@gmail.com>
2021-03-31 16:58:57 +0700

    Create TugasA.java

---
+++ Minggu 4/Tugas/TugasA.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author farul
+ */
+public class TugasA {
+    public static void main(String[]rgs) {
+        Scanner in = new Scanner(System.in);
+        int batasAwal, batasAkhir;
+        
+        System.out.println("Masukkan batas awal bilangan : ");
+        batasAwal = in.nextInt();
+        
+        System.out.println("Masukkan batas akhir bilangan : ");
+        batasAkhir = in.nextInt();
+        
+        for (int i = batasAwal; i <= batasAkhir; i++) {
+            System.out.print(" " + i);
+        }
+        
+            
+    }
+}
